e8dd6804ba3a130f85f1b8c8f221c8d15af2acb8
Gokul Patil <72352930+gokul-patil0509@users.noreply.github.com>
2021-05-25 15:14:30 +0530

    Add files via upload

---
+++ Diabetes_prediction/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.conf import settings
+
+
+urlpatterns = [
+     path('app/', include('Classifier_Web_App.urls')),
+]
